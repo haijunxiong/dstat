527d9056a7eff3e7a471122376204e86313f1871
haijunxiong <mengfg@gmail.com>
2012-12-30 22:23:41 +0800

    完成baseinfo

---
+++ baseinfo.py
@@ -0,0 +1,256 @@
+'''
+Created on 2012-12-25
+
+@author: mengjia
+'''
+import wmi,subprocess,re,json,_winreg
+
+w = wmi.WMI()
+
+def getWebServerInfoFromIIS6():
+    webserver = {}
+    sites=[]
+    siteindex = 1
+
+    try:
+        c = wmi.WMI(namespace="MicrosoftIISv2")
+    except:
+        return webserver
+
+    try:
+
+        for server in c.IIsWebServerSetting():
+            site = {}
+            ip = set()
+            domain = set()
+
+            site["index"] = siteindex
+            siteindex += 1
+            site["name"] = str(server.Name)
+
+            for bind in server.ServerBindings:
+                if(bind.hostname == ""):
+                    bind.hostname = "*"
+                    
+                if(bind.ip == ""):
+                    bind.ip = "*"   
+
+                domain.add(str(bind.hostname))
+                ip.add(str(bind.ip) +':'+ str(bind.port) )
+            
+            site["ip"] = list(ip)
+            site["domain"] = list(domain)
+            sites.append(site) 
+    except:
+        pass
+
+    webserver["type"] = "iis"
+    webserver["version"] = 6
+    webserver["sites"] = sites
+
+    return webserver
+
+def getWebServerInfoFromIIS7():
+
+    webserver = {}
+    sites=[]
+
+    try:
+        c = wmi.WMI(namespace="WebAdministration")
+    except:
+        return webserver    
+
+    try:
+        for site in c.site():
+
+            tsite = {}
+            ip = set()
+            domain = set()
+
+            tsite["index"] = site.Id
+            tsite["name"] = site.Name
+
+            
+            for bind in site.bindings:
+                if bind.protocol == "http" or bind.protocol == "https":
+                    bindinfo = bind.bindingInformation.split(":")
+                    if bindinfo[2] == "":
+                        bindinfo[2] = "*"
+
+                    domain.add(bindinfo[2])
+                    ip.add(bindinfo[0] +':'+ bindinfo[1] )
+
+
+            tsite["ip"] = list(ip)
+            tsite["domain"] = list(domain)
+            sites.append(tsite) 
+    except:
+        pass            
+
+    webserver["type"] = "iis"
+    webserver["version"] = 7
+    webserver["sites"] = sites
+    return webserver
+
+def getCpus():
+    cpus = []
+
+    try:
+        processors = w.Win32_Processor()
+        for p in processors:
+            cpu = {}
+            try:
+                cpu["name"] = p.name
+                cpu["clock"] = p.maxclockspeed
+                cpu["core"] = p.numberofcores
+                cpu["logicalprocessor"] = p.numberoflogicalprocessors
+            except AttributeError:
+                pass
+
+            cpus.append(cpu)
+    except:
+        pass
+
+    return cpus
+
+def getInstalledSoftFromWmi():
+    softs = set()
+
+    try:
+        products = w.Win32_Product()
+        for p in sorted(products,key = lambda d : d.caption):
+            softs.add(p.caption)
+    except:
+        pass        
+    
+    return list(softs)
+
+
+def getInstalledSoftFromRegistry():
+    softs = set()
+
+    try:
+        r = wmi.Registry ()
+        result, names = r.EnumKey (hDefKey=_winreg.HKEY_LOCAL_MACHINE, sSubKeyName=r"Software\Microsoft\Windows\CurrentVersion\Uninstall")
+
+        keyPath = r"Software\Microsoft\Windows\CurrentVersion\Uninstall"
+     
+        for subkey in names:
+            try:
+                path = keyPath + "\\" + subkey
+                key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, path, 0, _winreg.KEY_ALL_ACCESS) 
+                
+                temp = _winreg.QueryValueEx(key, 'DisplayName')
+                display = str(temp[0])
+                if display == '' or display.lower().startswith('hotfix'):
+                    continue
+
+                softs.add(display)                
+            except:
+                pass
+    except:
+        pass
+
+    return list(softs)
+
+def getDrives():
+    drives = []
+
+    try:
+        diskDrives = w.Win32_DiskDrive()
+        for d in diskDrives:
+            if d.size > 0:
+                drive = {}
+                drive["name"] = d.name
+                drive["interfacetype"] = d.interfacetype
+                drive["size"] = float(d.size)/(1024*1024)
+                drives.append(drive)
+    except:
+        pass
+
+    return drives
+
+def getMemory():
+    try:
+        computerSystem = w.Win32_ComputerSystem()[0]
+        return float(computerSystem.TotalPhysicalMemory)/(1024*1024)
+    except:
+        return 0
+
+def getPartition():
+    partitions = []
+
+    try:
+        disks = w.Win32_LogicalDisk (DriveType=3)
+        for d in disks:
+            partition = {}
+            partition["caption"] = d.caption
+            partition["filesystem"] = d.filesystem
+            partition["size"] = float(d.size)/(1024*1024)
+            partitions.append(partition)
+    except:
+        pass
+
+    return partitions    
+
+def getOsName():
+    try:
+        computerSystem = w.Win32_ComputerSystem()[0]
+        return computerSystem.name
+    except:
+        return ''
+
+
+def getOs():
+    os = {}
+
+    os["name"] = getOsName()
+    os["partition"] = getPartition()
+
+    softs = getInstalledSoftFromWmi()
+
+    if len(softs) <= 0:
+        softs = getInstalledSoftFromRegistry()
+    
+    os["soft"] = softs
+
+    webserver = getWebServerInfoFromIIS7()
+
+    if len(webserver) <= 0:
+        webserver = getWebServerInfoFromIIS6()
+
+    os["webserver"] = webserver
+
+    os["network"] =  getNetwork()
+
+    return os
+
+def getNetwork():
+    network = {}
+    try:
+        r = subprocess.check_output(['netstat','-an','-p','TCP'],shell=True)
+        ports = sorted( re.findall(':(\d+).+LISTENING',r),key=lambda d: float(d))
+
+        network["port"] = ports
+    except:
+        pass
+
+    return network
+
+def getBaseInfo():
+    info={}
+    hardware = {}         
+    hardware["cpu"] = getCpus()
+    hardware["memory"] = getMemory()
+    hardware["disk"] = getDrives()
+
+    software = {}
+    software["os"] = getOs()
+
+    info["hardware"] = hardware
+    info["software"] = software
+    return info
+
+if __name__ == '__main__':
+    print json.dumps(getBaseInfo())
+
--- test3.py
+++ test3.py
@@ -1,21 +1,19 @@
 import StringIO
 import traceback
 import wmi
-from _winreg import (HKEY_LOCAL_MACHINE, KEY_ALL_ACCESS, 
-                     OpenKey, EnumValue, QueryValueEx)
- 
+import _winreg
  
 r = wmi.Registry ()
-result, names = r.EnumKey (hDefKey=HKEY_LOCAL_MACHINE, sSubKeyName=r"Software\Microsoft\Windows\CurrentVersion\Uninstall")
+result, names = r.EnumKey (hDefKey=_winreg.HKEY_LOCAL_MACHINE, sSubKeyName=r"Software\Microsoft\Windows\CurrentVersion\Uninstall")
 
 keyPath = r"Software\Microsoft\Windows\CurrentVersion\Uninstall"
  
 for subkey in names:
     try:
         path = keyPath + "\\" + subkey
-        key = OpenKey(HKEY_LOCAL_MACHINE, path, 0, KEY_ALL_ACCESS) 
+        key = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, path, 0, _winreg.KEY_ALL_ACCESS) 
         try:
-            temp = QueryValueEx(key, 'DisplayName')
+            temp = _winreg.QueryValueEx(key, 'DisplayName')
             display = str(temp[0])
             if display == '' or display.lower().startswith('hotfix'):
                 continue
